d64ec44b54e7e228fabab9b149b6d19549b34931
kelvinlt <kelvinleethen@gmail.com>
2017-05-17 16:36:49 +0200

    Mejorar creacion de equipo y enseñar mejor los equipos con sus integrantes

--- public_html/javascript/controller.js
+++ public_html/javascript/controller.js
@@ -1,33 +1,47 @@
 var app = angular.module("myApp", []);
 
 app.controller("myCtrl", ["$scope", "serv"
-    , function ($scope, serv) {
-    var team = new Team("Souls Crusaders", 100, 0, "Dark Souls");
+            , function ($scope, serv) {
+                var team = new Team("Souls Crusaders", 100, 0, "Dark Souls");
 
-        var player1 = new Player("Kelvin", "player1", "Magician", 90,110);
-        var player2 = new Player("Lluis", "player2", "Paladin", 10,30);
-        var player3 = new Player("Danyl", "player3", "Warrior", 210,400);
-        var player4 = new Player("Marc", "player4", "Warrior", 910,200);
+                var player1 = new Player("Kelvin", "player1", "Magician", 90, 110);
+                var player2 = new Player("Lluis", "player2", "Paladin", 10, 30);
+                var player3 = new Player("Danyl", "player3", "Warrior", 210, 400);
+                var player4 = new Player("Marc", "player4", "Warrior", 910, 200);
 
-        team.addPlayer(player1);
-        team.addPlayer(player2);
-        team.addPlayer(player3);
-        team.addPlayer(player4);
-    
-        $scope.team = team;
-        $scope.player1 = player1;
-        
-        $scope.newTeam={};
-        $scope.newPlayer={};
-        
-        serv.addTeam(team);
-        
-        $scope.teamList = serv.teamList;
-        $scope.createTeam = function () {
-            if ($scope.newTeam.name !== undefined) {
-                var team = new Team($scope.newTeam.name, $scope.newTeam.victories, $scope.newTeam.defeats, $scope.newTeam.game);
+                team.addPlayer(player1);
+                team.addPlayer(player2);
+                team.addPlayer(player3);
+                team.addPlayer(player4);
+
+                $scope.team = team;
+
+                $scope.newTeam = {};
+                $scope.newPlayer = {};
+
                 serv.addTeam(team);
-            }
-        };
-    
-}]);
+                
+                var team1 = new Team("Profes", 200, 0, "Stucom");
+                var prof1 = new Player("Cristian", "prof1", "FrontEnd", 90, 110);
+                var prof2 = new Player("Alfredo", "prof2", "BackEnd", 10, 30);
+                var prof3 = new Player("Lluis", "prof3", "Servidores", 210, 400);
+                var prof4 = new Player("Diana", "prof4", "Tutora", 910, 200);
+
+                $scope.team1 = team1;
+                
+                team1.addPlayer(prof1);
+                team1.addPlayer(prof2);
+                team1.addPlayer(prof3);
+                team1.addPlayer(prof4);
+                
+                serv.addTeam(team1);
+                
+                
+                $scope.teamList = serv.teamList;
+                $scope.createTeam = function () {
+                    if ($scope.newTeam.name !== undefined) {
+                        var team = new Team($scope.newTeam.name, $scope.newTeam.victories, $scope.newTeam.defeats, $scope.newTeam.game);
+                        serv.addTeam(team);
+                    }
+                };
+            }]);
