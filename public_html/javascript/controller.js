6efd0bfaed3eb68d9ea9de84b4b36b9d962fb4b1
kelvinlt <kelvinleethen@gmail.com>
2017-05-17 16:43:33 +0200

    creacion de jugadores en equipo activo

--- public_html/javascript/controller.js
+++ public_html/javascript/controller.js
@@ -20,7 +20,7 @@ app.controller("myCtrl", ["$scope", "serv"
                 $scope.newPlayer = {};
 
                 serv.addTeam(team);
-                
+
                 var team1 = new Team("Profes", 200, 0, "Stucom");
                 var prof1 = new Player("Cristian", "prof1", "FrontEnd", 90, 110);
                 var prof2 = new Player("Alfredo", "prof2", "BackEnd", 10, 30);
@@ -28,20 +28,30 @@ app.controller("myCtrl", ["$scope", "serv"
                 var prof4 = new Player("Diana", "prof4", "Tutora", 910, 200);
 
                 $scope.team1 = team1;
-                
+
                 team1.addPlayer(prof1);
                 team1.addPlayer(prof2);
                 team1.addPlayer(prof3);
                 team1.addPlayer(prof4);
-                
+
                 serv.addTeam(team1);
-                
-                
+
+
                 $scope.teamList = serv.teamList;
                 $scope.createTeam = function () {
                     if ($scope.newTeam.name !== undefined) {
                         var team = new Team($scope.newTeam.name, $scope.newTeam.victories, $scope.newTeam.defeats, $scope.newTeam.game);
                         serv.addTeam(team);
+                    }
+                };
+                $scope.createPlayer = function () {
+                    if ($scope.teamList[$scope.activeTeam] !== undefined && $scope.newPlayer.nickname !== undefined) {
+                        var player = new Player($scope.newPlayer.name, $scope.newPlayer.nickname, $scope.newPlayer.position, $scope.newPlayer.points);
+                        if ($scope.teamList[$scope.activeTeam].players[player.nickname] === undefined) {
+                            $scope.teamList[$scope.activeTeam].addPlayer(player);
+                        } else {
+                            $scope.teamList[$scope.activeTeam].players[player.nickname] = player;
+                        }
                     }
                 };
             }]);
